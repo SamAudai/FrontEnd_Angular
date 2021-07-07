38de433636dffbf8ee4512aa04986423abe9f36a
Audai Alsh'arani <audaisam@hotmail.com>
2021-07-07 11:59:23 +0300

    Single Page Applications Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -8,8 +8,19 @@ import { DISHIES } from '../shared/dishes';
 export class DishService {
 
   constructor() { }
-
+  
+  //return all dishies
   getDishes(): Dish[] {
     return DISHIES;
+  }
+  
+  //return specific dish in array by id
+  getDish(id:string): Dish{
+    return DISHIES.filter((dish)=>(dish.id===id))[0];
+  }
+  
+  //return specific dish in array by featured
+  getFeaturedDish(): Dish{
+    return DISHIES.filter((dish) => dish.featured)[0];
   }
 }
