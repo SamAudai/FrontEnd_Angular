318b87bfee19ec1dcff29b8fc48082b9dd5fd18b
Audai Alsh'arani <audaisam@hotmail.com>
2021-07-09 11:50:55 +0300

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 
@@ -45,12 +49,17 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    FormsModule ,
     AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule    
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    MatDialogModule
+       
   ],
   providers: [
     DishService,
