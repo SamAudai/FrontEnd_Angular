416a57ebb8a9741a27a4a758b1289d75434c263d
Audai Alsh'arani <audaisam@hotmail.com>
2021-07-23 12:26:07 +0300

    Component Test

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -5,7 +5,7 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(public el: ElementRef, public renderer: Renderer2) { }
 
   @HostListener('mouseenter') onmouseenter() { 
     this.renderer.addClass(this.el.nativeElement, 'highlight');
